1adf13ff7e9b0065c810cdfd4f19575aa6d86dd1
kimgyuha <102589413+applely25@users.noreply.github.com>
2022-07-01 16:51:23 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,18 @@
+/*
+         ,r\'\"7
+r\`-_   ,\'  ,/
+ \\. \". L_r\'
+   \`~\\/
+      |
+      |
+*/
+
+#include<stdio.h>
+int main(){
+    printf("         ,r\'\"7\n");
+    printf("r\`-_   ,\'  ,/\n");
+    printf(" \\. \". L_r\'\n");
+    printf("   \`~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+}
