2464724fc235e02022e5b97124faa3c251e2149d
kimgyuha <102589413+applely25@users.noreply.github.com>
2022-07-03 13:56:54 +0900

    [Silver V] Title: 셀프 넘버, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int sum(int n)
+{
+    int sum = n;
+
+    while (n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+int main(void)
+{
+    int arr[10001], i, check;
+
+    for (i = 1; i < 10001; i++)
+    {
+        check = sum(i);
+        if (check < 10001)
+            arr[check] = 1;
+    }
+
+    for (i = 1; i < 10001; i++)
+    {
+        if (arr[i] != 1)
+            printf("%d\n", i);
+    }
+    return 0;
+}
