5116e649e573c798ef6988754cb4294f5ae67087
kimgyuha <102589413+applely25@users.noreply.github.com>
2022-09-19 16:18:54 +0900

    Update 한수.cc

--- 백준/Silver/1065. 한수/한수.cc
+++ 백준/Silver/1065. 한수/한수.cc
@@ -1,23 +1,23 @@
 #include <stdio.h>
-int Han(int n);
-int main() {
-    int input, res;
-    scanf("%d", &input);
-    res = Han(input);
-    printf("%d", res);
-}
-int Han(int n) {
-    int i, cnt = 0, hund, ten, one;
+int a(int n) {
+    int i, cnt = 0, h, t, o;
     if (n < 100)
         return n;
     else {
         for (i = 100; i <= n; i++) {
-            hund = i / 100;
-            ten = (i % 100) / 10;
-            one = (i % 100) % 10;
-            if ((hund - ten) == (ten - one))
+            h = i / 100;
+            t = (i % 100) / 10;
+            o = (i % 100) % 10;
+            if ((h - t) == (t - o))
                 cnt++;
         }
         return (99 + cnt);
     }
+}
+
+int main() {
+    int in, res;
+    scanf("%d", &in);
+    res = a(in);
+    printf("%d", res);
 }
