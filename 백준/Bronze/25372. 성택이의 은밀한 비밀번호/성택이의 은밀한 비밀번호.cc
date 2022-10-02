fdb999916163f7b00bd416e9626bb43d4a67b1eb
kimgyuha <102589413+applely25@users.noreply.github.com>
2022-10-03 00:18:53 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.cc
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+	int n;
+	char pw[20];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%s", pw);
+		if (strlen(pw) >= 6 && strlen(pw) <= 9)
+			printf("yes\n");
+		else printf("no\n");
+	}
+}
