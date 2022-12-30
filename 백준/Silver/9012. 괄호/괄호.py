ea7a4a1e5f0b9305341c3495db0b26fec8b75593
kimgyuha <102589413+applely25@users.noreply.github.com>
2022-12-30 20:09:05 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,18 +1,13 @@
 n = int(input())
 for i in range(n):
-    result = []
-    printResult = "YES"
     arr = list(input())
-    for j in range(len(arr)):
-        if(arr[j] == "("):
-            result.append(arr[j])
-        else:
-            if(len(result) == 0):
-                printResult="NO"
-                break;
-            if(result.pop() != "("):
-                printResult="NO"
-                break;
-    if(printResult == "YES" and len(result) == 0):
-        print("YES")
-    else: print("NO")
+    result = 0
+    for j in arr:
+        if(j == "("): result+=1
+        else: result-=1
+        if(result < 0):
+            print("NO")
+            break;
+    if(result > 0):
+        print("NO")
+    elif(result == 0): print("YES")
