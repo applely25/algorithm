27bb78087c0026ed45fc36b152b5d59795d3f007
kimgyuha <102589413+applely25@users.noreply.github.com>
2023-04-06 16:00:52 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,8 @@
+function solution(numbers, direction) {
+    if(direction == "left"){
+        numbers.push(numbers.shift())
+    }else{
+        numbers.unshift(numbers.pop())
+    }
+    return numbers;
+}
