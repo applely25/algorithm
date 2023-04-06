7b6bdc18e0e11f090c8b132de60ee18e0f3a82d0
kimgyuha <102589413+applely25@users.noreply.github.com>
2023-04-06 15:48:40 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,5 @@
+function solution(my_string, num1, num2) {
+    my_string = my_string.split("");
+    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+    return my_string.join("");
+}
