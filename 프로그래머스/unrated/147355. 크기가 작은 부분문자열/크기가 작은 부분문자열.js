b2442755000743d39100d06a72a73c99f2488360
kimgyuha <102589413+applely25@users.noreply.github.com>
2023-06-27 15:21:06 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,15 +1,19 @@
 function solution(t, p) {
     let num = [...t];
-    let len = [...p].length;
+    let len = p.length;
     let result = [];
     
     for(let i = 0; i<=num.length - len; i++){
+        /* for문 반복하면서 숫자를 찾고 join해서 result 배열에 삽입
         let number = [];
         for(let j = 0; j<len; j++){
            number.push(num[i+j])
         }
         result.push(Number(number.join("")))
+        */
+        // slice해서 숫자 찾기
+        result.push(num.slice(i, i+len).join(""))
     }
-    result = result.filter(n=> n<=Number(p)) 
+    result = result.filter(n=> n<=p) 
     return result.length
 }
