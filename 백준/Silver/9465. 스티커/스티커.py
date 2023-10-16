13a121fe585677d91c800ebb0eb00519781bf8b2
kimgyuha <102589413+applely25@users.noreply.github.com>
2023-10-16 19:03:15 +0900

    [Silver I] Title: 스티커, Time: 1168 ms, Memory: 58032 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,18 +1,15 @@
 for _ in range(int(input())):
-  size = int(input())
-  sticker = [[0]*2 for _ in range(size)]
-  d = [[0]*3 for _ in range(size)]
+  sticker = [0,0]
+  d = [[0]*3 for _ in range(int(input()))]
 
-  for i in range(2):
-    dList = list(map(int, input().split()))
-    for j in range(size):
-      sticker[j][i] = dList[j]
+  sticker[0] = list(map(int, input().split()))
+  sticker[1] = list(map(int, input().split()))
   
-  d[0] = [0, sticker[0][0], sticker[0][1]]
+  d[0] = [0, sticker[0][0], sticker[1][0]]
 
-  for i in range(1, size):
+  for i in range(1, len(d)):
     d[i][0] = max(d[i-1])
-    d[i][1] = sticker[i][0] + max(d[i-1][0], d[i-1][2])
-    d[i][2] = sticker[i][1] + max(d[i-1][0], d[i-1][1])
+    d[i][1] = sticker[0][i] + max(d[i-1][0], d[i-1][2])
+    d[i][2] = sticker[1][i] + max(d[i-1][0], d[i-1][1])
 
-  print(max(d[size-1]))
+  print(max(d[-1]))
